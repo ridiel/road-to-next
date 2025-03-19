d03f84c23cfaef036373c0c0b9a2b17fa0ae4d17
dmytro <kuzovlev.dev@gmail.com>
2025-03-19 17:47:34 +0100

    made a progress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"], // React and external packages
+            ["^@/"], // Aliased imports (e.g., `@/components`)
+            ["^\\.\\./?$", "^\\./(.*)$"], // Relative imports
+            ["^.+\\.s?css$"], // Styles
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    }
+  }
 ];
 
 export default eslintConfig;
